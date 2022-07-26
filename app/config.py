3235ddfd09c604179d35751751acee5d5a8eac41
jammel-yeboah <93840983+jammel-yeboah@users.noreply.github.com>
2022-07-26 03:07:26 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -12,5 +12,5 @@ class Config():
     SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
 
     # This will create a file in <app> FOLDER
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
