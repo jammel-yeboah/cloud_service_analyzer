5115faaf79288984edcea3f6fd88d50b1ac7064b
jammel-yeboah <93840983+jammel-yeboah@users.noreply.github.com>
2022-07-26 03:12:45 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -13,4 +13,6 @@ class Config():
 
     # This will create a file in <app> FOLDER
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
